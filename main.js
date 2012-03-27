5646e19c46477939706d3a53e5b30488f00fc834
haja <h.jagenteufel@gmail.com>
2012-03-27 22:16:32 +0200

    oop version of advanced game loop

--- main.js
+++ main.js
@@ -1,21 +1,47 @@
-init();
-var ctx;
-
 var HORZ_SIZE = 800;
 var VERT_SIZE = 450;
 var TILE_SIZE = 50;
 
-function init() {
-	var canvas = document.getElementById("canvas");  
-	ctx = canvas.getContext("2d");  
-	draw();
+
+
+function Game() {
+	this.fps = 48;
+	this.startX = 20;
+	this.startY = 20;
+	this.tileSize = 50;
+	this.ctx = document.getElementById("canvas").getContext("2d");
+
+	this.looping = new Object();
+	this.looping.skipTicks = 1000 / this.fps;
+	this.looping.maxFrameSkip = 10;
+	this.looping.nextGameTick = (new Date).getTime();
 }
 
+Game.prototype.update = function() {
+	this.startX = this.startX + 1;
+};
 
-function draw() {
-	ctx.fillStyle = "gray";  
-	ctx.fillRect(0, 0, 800, 450);  
-	ctx.fillStyle = "red";  
-	ctx.fillRect(50, 50, 50, 50);  
-}  
+Game.prototype.draw = function() {
+	this.ctx.fillStyle = "gray";
+	this.ctx.fillRect(0, 0, 800, 450);
+	this.ctx.fillStyle = "red";
+	this.ctx.fillRect(this.startX, this.startY, this.tileSize, this.tileSize);
+};
+
+Game.prototype.run = function() {
+		var loops = 0;
+
+		while ((new Date).getTime() > this.looping.nextGameTick && loops < this.looping.maxFrameSkip) {
+			this.update();
+			this.looping.nextGameTick += this.looping.skipTicks;
+			loops++;
+		}
+
+		this.draw();
+};
+
+var myGame = new Game();
+
+// Start the game loop
+myGame._intervalID = setInterval("myGame.run()", 1000 / myGame.fps);
 
